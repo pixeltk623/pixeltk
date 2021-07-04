789bc30a59c9c118914ccb1e40decc63429925e5
chintan623 <chintan@pixeltk.com>
2021-07-04 17:20:42 +0530

    Update main.component.ts

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -13,13 +13,30 @@ export class MainComponent implements OnInit {
   constructor(private route:Router) { 
     //console.log(environment.baseUrl);
   }
+  
+   public status:boolean=false;
 
   ngOnInit(): void {
+    
   }
 
   angularCourse() {
     //console.log("Hello")
     this.route.navigate(['courses/angular-certification-training-course']);
   }
+
+  login(){
+     console.log('hello');
+     this.status=true;
+  }
+
+  logOut(){
+     this.status=false;
+  }
+
+  submit(){
+    this.status=false;
+  }
+  
 
 }
