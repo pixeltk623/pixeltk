eb1d9e6120265d66b1fee631017be676facfaa6a
chintan623 <chintan@pixeltk.com>
2021-07-26 22:11:36 +0530

    LARAVEL page form validation

--- src/app/laravel-certification-training-course/laravel-certification-training-course.component.ts
+++ src/app/laravel-certification-training-course/laravel-certification-training-course.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
+import { Courses } from '../courses';
 
 @Component({
   selector: 'app-laravel-certification-training-course',
@@ -10,7 +11,16 @@ export class LaravelCertificationTrainingCourseComponent implements OnInit {
   public baseUrl:string = environment.firebase.baseUrl;
   constructor() { }
 
+  modelOfCourses = new Courses('','');
+
+  isdisabled:boolean = true;
+
+  onSubmit(){
+    console.log(this.modelOfCourses);
+  }
+
   ngOnInit(): void {
+    console.log(this.modelOfCourses)
   }
 
 }
